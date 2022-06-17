6499c2633c0c3afec1dffac4a94271303f1d1bd8
DESKTOP-V6HUDEC\user <dbd02017@gmail.com>
2022-06-17 16:01:53 +0900

    no message

---
+++ my_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'time_attack_0617',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+SECRET_KEY = 'django-insecure-p@=74m3(c6h1-j_a(lsyra)_cqdnbrfij!mvg3chyx%4_e)g_p'
